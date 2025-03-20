2f2d7248c7707434bc9a4c43f136e5aaf3d71c01
jkchinnu444444@gmail.com <jkchinnu444444@gmail.com>
2025-03-20 11:05:01 +0530

    minor changes

--- src/app/features/predict/page.tsx
+++ src/app/features/predict/page.tsx
@@ -3,6 +3,7 @@
 import Template from "@/components/ui/transition";
 import { motion } from "framer-motion";
 import { PlaceholdersAndVanishInput } from "@/components/ui/InputBox";
+import Link from "next/link";
 export default function Feature1() {
   const placeholders = [
     "The food at that restaurant was amazing!",
@@ -39,6 +40,36 @@ export default function Feature1() {
   };
   return (
     <Template>
+      <motion.div
+        initial={{ x: -100, opacity: 0, scale: 0.8 }}
+        animate={{ x: 0, opacity: 1, scale: 1 }}
+        transition={{ duration: 1 }}
+        whileHover={{ scale: 1.05 }}
+        className="ml-8 pt-6 "
+      >
+        <Link href="/features">
+          <button className="inline-flex items-center justify-center cursor-pointer gap-2 text-neutral-400 italic  text-2xl">
+            <svg
+              data-slot="icon"
+              fill="none"
+              width="24"
+              height="24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+              ></path>
+            </svg>
+            Go Back
+          </button>
+        </Link>
+      </motion.div>
       <div className="flex justify-center">
         {text.split(" ").map((cur, i) => (
           <motion.div
